26eb19e2b7271a779c542279fda292e10c33c13b
leapm <graphql.js@gmail.com>
2021-01-06 02:01:21 +0800

    support withdraw

--- scripts/arbi.status.ts
+++ scripts/arbi.status.ts
@@ -1,4 +1,5 @@
 import { Contract, utils, providers, Wallet, ethers, BigNumber } from 'ethers'
+import deployer from '../secret/deployer.json'
 import COREArbi from '../abis/COREArbi.json'
 import UniV2Router from '@uniswap/v2-periphery/build/UniswapV2Router02.json'
 import {
@@ -19,10 +20,12 @@ import {
 import { ONE_BTC } from '../constants/baseUnits'
 import { getErc20Instance } from './utils'
 import { Signer } from 'crypto'
-const { parseEther, formatEther } = utils
+const { parseEther, formatEther, formatUnits } = utils
 
 const provider = new providers.InfuraProvider(undefined, '3ad5fab786964809988a9c7fefc5d3a5')
 const router = new Contract(ROUTER, UniV2Router.abi, provider)
+const signer = new Wallet(deployer.key, provider)
+const coreArbi = new Contract(COREARBI, COREArbi.abi, signer)
 const initialEth = parseEther('6')
 const initialDai = parseEther('20000')
 
@@ -31,11 +34,23 @@ async function getEthPrice() {
   return buyPrice
 }
 
+async function getBtcPriceInEth() {
+  const [buyPrice] = await router.getAmountsIn(ONE_BTC, [WETH, WBTC])
+  return buyPrice
+}
 async function getEtherBalance() {
   const balance = await provider.getBalance(BOT)
   return balance
 }
 
+async function getBTCBalance() {
+  const wbtcInstance = getErc20Instance(WBTC, provider)
+  const wbtcBalance = (await wbtcInstance.balanceOf(COREARBI)) as BigNumber
+  const wwbtcInstance = getErc20Instance(WWBTC, provider)
+  const wwbtcBalance = (await wwbtcInstance.balanceOf(COREARBI)) as BigNumber
+  const totalBtc = wbtcBalance.add(wwbtcBalance)
+  return totalBtc
+}
 async function getDaiBalance() {
   const daiInstance = getErc20Instance(DAI, provider)
   const daiBalance = (await daiInstance.balanceOf(COREARBI)) as BigNumber
@@ -45,11 +60,17 @@ async function getDaiBalance() {
   const ethPrice = await getEthPrice()
   const profitInEth = daiProfit.mul(parseEther('1')).div(ethPrice)
   const leftEth = await getEtherBalance()
-  const allEth = profitInEth.add(leftEth)
+  const totalBtc = await getBTCBalance()
+  const btcPriceInEth = await getBtcPriceInEth()
+  const btcInEth = totalBtc.mul(btcPriceInEth).div(ONE_BTC)
+  const allEth = profitInEth.add(leftEth).add(btcInEth)
+
   console.log(
-    `daiProfit: ${formatEther(daiProfit)} ethPrice: ${formatEther(ethPrice)} ethProfit: ${formatEther(
-      profitInEth
-    )} leftEth: ${formatEther(leftEth)} totalEth: ${formatEther(allEth)}, profit: ${formatProfit(initialEth, allEth)}`
+    `daiProfit: ${formatEther(daiProfit)}, btcProfit: ${formatUnits(totalBtc.mul(10), 'gwei')} ethPrice: ${formatEther(
+      ethPrice
+    )} daiToEthProfit: ${formatEther(profitInEth)}, btcToEthProfit:${formatEther(btcInEth)} , leftEth: ${formatEther(
+      leftEth
+    )} totalEth: ${formatEther(allEth)}, profit: ${formatProfit(initialEth, allEth)}`
   )
 }
 function formatProfit(initialEth: BigNumber, currentBalance: BigNumber) {
@@ -58,7 +79,11 @@ function formatProfit(initialEth: BigNumber, currentBalance: BigNumber) {
     : '+' + formatEther(currentBalance.sub(initialEth))
 }
 
+async function withdrawProfit() {
+  await coreArbi.withdrawTokens([WBTC, DAI], [ONE_BTC, parseEther('5000')], deployer.address)
+}
 const functionToRun = getDaiBalance
+// const functionToRun = withdrawProfit
 functionToRun()
   .then((v: any) => {
     process.exit(0)
